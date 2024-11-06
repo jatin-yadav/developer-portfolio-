a2662030a05fc7fa714a60298fa9aba2dd28a909
jatin-yadav <jatinyadav19oct@gmail.com>
2024-11-06 22:22:33 +0530

    skills page grid update

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -2,6 +2,20 @@ import { cssLogo, dockerLogo, figmaLogo, gitLogo, htmlLogo, javascriptLogo, mong
 import SkillsBox from "@/components/SkillsBox"
 import { styles } from "@/styles"
 
+const skillsSet = [
+    { skill: "CSS", src: cssLogo },
+    { skill: "Doker", src: dockerLogo },
+    { skill: "Figma", src: figmaLogo },
+    { skill: "GitHub", src: gitLogo },
+    { skill: "HTML", src: htmlLogo },
+    { skill: "JavaScript", src: javascriptLogo },
+    { skill: "MongoDB", src: mongodbLogo },
+    { skill: "ExpressJS", src: nodejsLogo },
+    { skill: "ReactJS", src: reactjsLogo },
+    { skill: "Redux", src: reduxLogo },
+    { skill: "TailwindCss", src: tailwindLogo },
+    { skill: "TypeScript", src: typescriptLogo },
+]
 const Skills = () => {
     return (
         <section id="skills" className="pt-12 bg-secondary">
@@ -9,42 +23,13 @@ const Skills = () => {
                 <h1 className="text-4xl tracking-wide font-bold">My Skills</h1>
                 {/* <div className="max-w-7xl w-full flex flex-col sm:flex-row justify-between items-center  pt-16 gap-4 "> */}
                 <div className="max-w-7xl w-full pt-16 grid grid-cols-12">
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='CSS' imgSrc={cssLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='Doker' imgSrc={dockerLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='Figma' imgSrc={figmaLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='GitHub' imgSrc={gitLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='HTML' imgSrc={htmlLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='JavaScript' imgSrc={javascriptLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='MongoDB' imgSrc={mongodbLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='ExpressJS' imgSrc={nodejsLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='ReactJS' imgSrc={reactjsLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='Redux' imgSrc={reduxLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='TailwindCss' imgSrc={tailwindLogo} />
-                    </div>
-                    <div className="lg:col-span-3 md:col-span-4 col-span-6 md:p-8 p-3">
-                        <SkillsBox skill='TypeScript' imgSrc={typescriptLogo} />
-                    </div>
+                    {skillsSet.map(({ skill, src }) => {
+                        return (
+                            <div className="xl:col-span-2 lg:col-span-3 col-span-6 md:p-2 p-1">
+                                <SkillsBox skill={skill} imgSrc={src} />
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </section>
