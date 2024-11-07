c1917dfed29351511bf3ae53d46d46e08063cc0a
jatin-yadav <jatinyadav19oct@gmail.com>
2024-11-07 16:49:04 +0530

    little changes

--- src/components/SkillsBox.tsx
+++ src/components/SkillsBox.tsx
@@ -1,6 +1,6 @@
 const SkillsBox = ({ skill, imgSrc }: { skill: string, imgSrc: string }) => {
     return (
-        <div className="border-2 border-background-2 rounded-md p-4 flex flex-col justify-center items-center bg-background-2">
+        <div className="border-2 border-background-2 rounded-xl p-4 flex flex-col justify-center items-center bg-background-2">
             <div className="p-4 font-extrabold grayscale-0 hover:grayscale">
                 <img src={imgSrc} className="md:h-32 h-24" alt={skill} />
             </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -10,15 +10,16 @@ const About = () => {
     const theme = useRecoilValue(themesAtom)
     return (
         <section id='about'>
-            <div className={`${styles.paddingX} ${styles.paddingY} bg-background-1 flex items-center justify-center`}>
+            <div className={`${styles.paddingX} ${styles.paddingY} bg-background-1 flex flex-col items-center justify-center text-content`}>
                 <div className="max-w-7xl flex flex-col sm:flex-row justify-center items-center  pt-16 gap-4">
                     <div className="w-full lg:w-5/12">
                         <div className={`${theme === 'dark' ? 'bg-primary' : ""} rounded-md w-96`}>
                             <img src={aboutImg} alt="aboutImg" />
                         </div>
                     </div>
-                    <div className="w-full lg:w-7/12 text-content px-6">
-                        <h1 className="text-4xl tracking-wide font-bold py-4">About Me</h1>
+                    <div className="w-full lg:w-7/12 px-6">
+                        {/* <h1 className="text-xl tracking-wide lg:text-start text-center font-bold">About Me</h1> */}
+                        <h1 className="text-4xl tracking-wide font-bold py-4">Curious about me? Here you have it:</h1>
                         <p className="py-2">I am a Full Stack Developer with over 2.5 years of experience in designing and developing web applications. My expertise lies in the MERN stack, with a deep understanding of both front-end and back-end technologies such as React, Node.js, Express.js, and MongoDB. I am passionate about building scalable, high-performance applications that provide seamless user experiences.</p>
                         <p className="py-2">With a solid foundation in JavaScript, TypeScript, Next.js, Tailwind CSS, and database management using MySQL and PostgreSQL, I am continuously looking for opportunities to contribute to innovative projects that drive technological advancement. My work is fueled by a passion for clean code, enhanced user experience, and the pursuit of technical excellence.</p>
                         <div className="flex gap-4 py-6">
